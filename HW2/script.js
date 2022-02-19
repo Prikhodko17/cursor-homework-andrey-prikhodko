174485aa1bd47245afb14e8782995f7f1043d967
Andrey <andreprikhodko1997@gmail.com>
2022-02-19 21:50:06 +0200

    done HW2

--- HW2/script.js
+++ HW2/script.js
@@ -1,1 +1,21 @@
-console.log(12)
+do {
+    n = +prompt("Введіть число від якого ми будемо складати", 1);
+}while (n % 1 !== 0);
+    
+
+do {
+    m = +prompt("Введіть число до якого ми будемо складати", 2);
+}while (m < n || m % 1 !== 0);
+    
+const skipPairs = confirm("Пропускать парные числа?");
+
+let summary = 0;
+
+for (let i = n; i <= m; i++) {
+    if (skipPairs && i % 2 == 0) {
+        continue;
+    }
+    summary += i;
+}
+
+alert(`Сумма чисел між ${n} і ${m} складає ${summary}`)
