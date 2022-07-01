2069e2590ef579aa4568e427ae122ddec3968cc3
Prikhodko17 <andreprikhodko1997@gmail.com>
2022-07-01 19:48:41 +0300

    done HW10

--- HW10/script.js
+++ HW10/script.js
@@ -2,10 +2,15 @@ const letters = document.querySelectorAll(".letter")
 const audios = document.querySelectorAll('audio')
 
 function word () {
+    let divD = this
     const dataKey = this.attributes[1].textContent
     audios.forEach(el => {
         if (dataKey === el.id) {
            elem = document.getElementById(`${dataKey}`)
+           divD.classList.add("active")
+           setTimeout(() => {
+            divD.classList.remove("active");
+          }, 800)
            elem.play()
         }
     })
@@ -18,9 +23,18 @@ letters.forEach (el => {
 document.onkeydown = function (event) {
     audios.forEach(el => {
        if (event.keyCode == el.id) {
+        letters.forEach(e => {
+        if (e.attributes[1].textContent === el.id) {
         let sound = event.keyCode
         elem = document.getElementById(`${sound}`)
+        e.classList.add("active")
+        setTimeout(() => {
+        e.classList.remove("active");
+        }, 800)
         elem.play(0)
+        }
+        
+        })
        }
     }
         )
